73c1f0576ceefd0805deecf101525670ea669aca
Nwachukwu Clinton <nwachukwuclinton2@gmail.com>
2024-10-05 18:17:37 +0100

    initial commit

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from cloudinary.models import CloudinaryField
 
 
 
@@ -16,7 +17,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = CloudinaryField('image')
     body = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
